e7769c7ed96647b62918c164621ae7a2cf404a1e
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-03-23 18:17:28 -0300

    refactor

--- src/Services/SharedKernel/SalesSystem.SharedKernel/Messages/CommonMessages/IntegrationEvents/IntegrationEvent.cs
+++ src/Services/SharedKernel/SalesSystem.SharedKernel/Messages/CommonMessages/IntegrationEvents/IntegrationEvent.cs
@@ -2,6 +2,5 @@
 {
     public abstract record IntegrationEvent : Event
     {
-
     }
 }
--- src/Web/SalesSystem.API/Controllers/SalesController.cs
+++ src/Web/SalesSystem.API/Controllers/SalesController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Mvc;
 using SalesSystem.SharedKernel.Communication.Mediator;
 
 namespace SalesSystem.API.Controllers
@@ -6,5 +6,8 @@ namespace SalesSystem.API.Controllers
     [Route("api/v1/sales")]
     public class SalesController(IMediatorHandler mediatorHandler) : MainController
     {
+        [HttpGet]
+        public async Task<IResult> GetOrderByCustomerAsync()
+            => CustomResponse(await mediatorHandler.SendQuery(new Get))
     }
 }
