b8134587a43b8ddde7b00ef35bb5b54fc9107f09
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-05-27 15:08:35 -0300

    Update SignInUserValidation.cs

--- src/Services/Registers/SalesSystem.Registers.Application/Commands/Authentication/SignIn/SignInUserValidation.cs
+++ src/Services/Registers/SalesSystem.Registers.Application/Commands/Authentication/SignIn/SignInUserValidation.cs
@@ -12,24 +12,7 @@ namespace SalesSystem.Registers.Application.Commands.Authentication.SignIn
               .EmailAddress().WithMessage("Invalid email format.");
 
             RuleFor(x => x.Password)
-                .NotEmpty().WithMessage("The password field cannot be empty.")
-                .MinimumLength(8).WithMessage("The password must be at least 8 characters long.")
-                .Must(HasUpperCase).WithMessage("The password must contain at least one uppercase letter.")
-                .Must(HasLowerCase).WithMessage("The password must contain at least one lowercase letter.")
-                .Must(HasDigit).WithMessage("The password must contain at least one digit.")
-                .Must(HasSpecialCharacter).WithMessage("The password must contain at least one special character (!@#$%^&* etc.).");
-        }
-
-        private static bool HasUpperCase(string password) => password.Any(char.IsUpper);
-
-        private static bool HasLowerCase(string password) => password.Any(char.IsLower);
-
-        private static bool HasDigit(string password) => password.Any(char.IsDigit);
-
-        private static bool HasSpecialCharacter(string password)
-        {
-            var specialCharRegex = new Regex(@"[!@#$%^&*(),.?""{}|<>]");
-            return specialCharRegex.IsMatch(password);
+                .NotEmpty().WithMessage("The password field cannot be empty.");
         }
     }
 }
